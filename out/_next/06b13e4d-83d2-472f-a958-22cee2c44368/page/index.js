3c816480cac940e3e4cf1c9a9954af9d88095c1c
Donald Hruska <donaldhruska@gmail.com>
2017-10-13 00:27:58 -0500

    Add out

---
+++ out/_next/06b13e4d-83d2-472f-a958-22cee2c44368/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([6],{230:function(e,t,u){e.exports=u(231)},231:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(0),o=n(l),r=u(71),f=n(r);t.default=function(){return o.default.createElement(f.default,null)}}},[230]);
+            return { page: comp.default }
+          })
+        
