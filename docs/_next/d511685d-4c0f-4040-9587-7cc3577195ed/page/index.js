31a0399e0d378ddd5baa28de63fb0552bc864860
Donald Hruska <donaldhruska@gmail.com>
2017-11-04 17:42:12 -0500

    chore: regen site

--- docs/_next/d511685d-4c0f-4040-9587-7cc3577195ed/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([6],{231:function(e,t,u){e.exports=u(232)},232:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(0),o=n(l),r=u(45),f=n(r);t.default=function(){return o.default.createElement(f.default,null)}}},[231]);
-            return { page: comp.default }
-          })
-        
