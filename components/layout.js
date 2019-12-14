e99003245addf19d8e05b9ebaf53eb44ba6590c2
Donald Hruska <donaldhruska@gmail.com>
2019-12-13 21:46:52 -0800

    update meta

--- components/layout.js
+++ components/layout.js
@@ -6,11 +6,7 @@ import Fonts from "../general/fonts";
 import CssReset from "./cssReset";
 import GAWrapper from "./gaWrapper";
 import ActiveLink from "./activeLink";
-import {
-  BACKGROUND_COLOR,
-  HEADER_TEXT_COLOR,
-  FONT_FAMILY
-} from "../config/css.config";
+import { BACKGROUND_COLOR, HEADER_TEXT_COLOR, FONT_FAMILY } from "../config/css.config";
 
 class Layout extends React.Component {
   constructor(props) {
@@ -27,54 +23,37 @@ class Layout extends React.Component {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <title>{this.props.title}</title>
           <meta
             name="description"
-            content="Software engineer from Chicago, IL and Co-founder of Draftbit. Interested in building high performance, data-driven applications."
+            content="Donald Hruska ia a software engineer and the Co-founder of Draftbit"
           />
           <meta
             name="keywords"
-            content="Software engineering, Draftbit, Chicago, JavaScript, HTML, CSS, Python, Java, C#, University of Illinois at Urbana-Champaign"
+            content="Software engineering, Draftbit, Startups, Y Combinator, San Francisco, University of Illinois at Urbana-Champaign"
           />
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
           <link
             rel="canonical"
             href={`https://d.onald.org${
-              this.props.router.pathname !== "/"
-                ? this.props.router.pathname
-                : ""
+              this.props.router.pathname !== "/" ? this.props.router.pathname : ""
             }`}
           />
 
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://d.onald.org" />
           <meta property="og:title" content="Donald Hruska" />
-          <meta
-            property="og:image"
-            content="https://d.onald.org/static/profilePhoto.jpg"
-          />
-          <meta
-            property="og:description"
-            content="Donald Hruska's personal site"
-          />
+          <meta property="og:image" content="https://d.onald.org/static/profilePhoto.jpg" />
+          <meta property="og:description" content="Donald Hruska's personal site" />
           <meta property="og:locale" content="en_US" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:creator" content="@donaldhruska" />
           <meta name="twitter:url" content="https://d.onald.org" />
           <meta name="twitter:title" content="Donald Hruska" />
-          <meta
-            name="twitter:description"
-            content="Donald Hruska's personal site"
-          />
-          <meta
-            name="twitter:image"
-            content="https://d.onald.org/static/profilePhoto.jpg"
-          />
+          <meta name="twitter:description" content="Donald Hruska's personal site" />
+          <meta name="twitter:image" content="https://d.onald.org/static/profilePhoto.jpg" />
           <meta
             name="google-site-verification"
             content="Wv9dPEU8Ab4cIKSRWzJT4jLY_ZLP45RsxCHWXmx9sQo"
@@ -96,7 +75,9 @@ class Layout extends React.Component {
                 <ActiveLink href="/contact">Contact</ActiveLink>
               </li>
               <li>
-                <a href="https://blog.d.onald.org" target="_blank" rel="noopener">Blog</a>
+                <a href="https://blog.d.onald.org" target="_blank" rel="noopener">
+                  Blog
+                </a>
               </li>
             </ul>
           </nav>
